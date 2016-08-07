6f301ee323687f892bd3fc67007e3f58f00aca80
Mike M <agentmoller001@gmail.com>
2016-08-07 09:08:17 -0700

    Removed ClientArea Function

--- modules/servers/ispconfig3/ispconfig3.php
+++ modules/servers/ispconfig3/ispconfig3.php
@@ -142,20 +142,6 @@ function ispconfig3_ChangePackage($params) {
 
 }
 
-function ispconfig3_ClientArea($params) {
-
-    # Output can be returned like this, or defined via a clientarea.tpl template file (see docs for more info)
-
-	$code = '<form action="http://'.$serverip.'/controlpanel" method="post" target="_blank">
-<input type="hidden" name="user" value="'.$params["username"].'" />
-<input type="hidden" name="pass" value="'.$params["password"].'" />
-<input type="submit" value="Login to Control Panel" />
-<input type="button" value="Login to Webmail" onClick="window.open(\'http://'.$serverip.'/webmail\')" />
-</form>';
-	return $code;
-
-}
-
 function ispconfig3_AdminLink($params) {
 
 	$code = '<form action=\"http://'.$params["serverip"].'/controlpanel" method="post" target="_blank">
